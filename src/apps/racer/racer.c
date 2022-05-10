e3490427d5de409da535e86d0e55c0ec2f7f6101
Mark <mcg56@uclive.ac.nz>
2022-05-11 10:39:49 +1200

    Milestone 2 - Rough PWM control

--- src/apps/racer/racer.c
+++ src/apps/racer/racer.c
@@ -3,6 +3,7 @@
 #include "pacer.h"
 
 #define LED_FLASH_RATE 10
+#define PACER_RATE 1000
 
 /* TODO
 - Always specify size eg (uint8_t)
@@ -14,12 +15,86 @@
 - Broadcast many packets when bumper hit to ensure hat recieves
 */
 
+static usb_serial_cfg_t usb_serial_cfg =
+{
+    .read_timeout_us = 1,
+    .write_timeout_us = 1,
+};
+
+static usb_serial_t *usb_serial;
+
+
+static void
+prompt_command (void)
+{
+    usb_serial_puts (usb_serial, "> ");
+}
+
+
+static void
+process_command (void)
+{
+    char buffer[80];
+    char *str;
+    
+    str = usb_serial_gets (usb_serial, buffer, sizeof (buffer));
+    if (! str)
+        return;
+
+    //usb_serial (usb_serial, "<<<%s>>>\n", str);
+    //usb_serial_puts (usb_serial, str);
+    //usb_serial_puts (usb_serial, str[1]);
+    
+
+    switch (str[0])
+    {
+    case 'f':
+        usb_serial_puts (usb_serial, "Forward!\n");
+        set_motor_vel (0, 70);
+        break;
+
+    case 'b':
+        usb_serial_puts (usb_serial, "Backward!\n");
+        set_motor_vel (0, -70);
+        break;
+    case 'l':
+        usb_serial_puts (usb_serial, "Left!\n");
+        set_motor_vel (20, 50);
+        break;
+        
+    case 'r':
+        usb_serial_puts (usb_serial, "Right!\n");
+        set_motor_vel (-20, 50);
+        break;
+
+    case 's':
+        usb_serial_puts (usb_serial, "Stop!\n");
+        set_motor_vel (0, 0);
+        break;
+
+    default:
+       break;
+    }
+
+    prompt_command ();
+}
 
 
 int
 main (void)
 {
-    uint8_t i = 0;
+    //Milesetone 2 butchery
+    usb_cdc_t usb_cdc;
+    int flash_ticks = 0;
+    int i;
+    usb_serial = usb_serial_init (&usb_serial_cfg, "");
+
+    prompt_command ();
+    
+    
+    pio_config_set (LED_STATUS_PIO, PIO_OUTPUT_HIGH);
+    pacer_init (LED_FLASH_RATE * 2);
+    //uint8_t i = 0;
     //---------------------Peripheral setup---------------------
     init_pwm (); // Initalise PWM outputs
 
@@ -30,14 +105,25 @@ main (void)
     //pacer_init (LED_FLASH_RATE*2);
 
     // Redirect stdio to USB serial
-    usb_serial_stdio_init ();
-    set_motor_vel (20, 70);
+    //usb_serial_stdio_init ();
+    set_motor_vel (0, 0);
 
     while (1) {
+        pacer_wait ();
+        flash_ticks++;
+        if (flash_ticks >= PACER_RATE / (LED_FLASH_RATE * 2)) {
+            flash_ticks = 0;
+
+	        pio_output_toggle (LED_STATUS_PIO);
+
+            process_command ();
+        }
+	}
+
         //pacer_wait ();
         //set_motor_vel (0, 100);
         //pio_output_toggle (LED_STATUS_PIO);
         //pio_output_toggle (LED_ERROR_PIO);
         //printf ("Hello world %d\n", i++);
-    }
+    
 }
